890c1596043d0e95348b8828c1181e722c7210c1
Chris Meyering <christophe.meyering@gmail.com>
2020-02-11 12:29:08 -0800

    implemented basic block functionality

---
+++ block.js
@@ -0,0 +1,27 @@
+const {GENESIS_DATA} = require('./config');
+const cryptoHash = require('./crypto-hash');
+
+class Block {
+    constructor({timestamp, lastHash, hash, data}) {
+        this.timestamp = timestamp;
+        this.lastHash = lastHash;
+        this.hash = hash;
+        this.data = data;
+    }
+
+    static genesis() {
+        return new this(GENESIS_DATA);
+    }
+
+    static mineBlock({lastBlock, data}) {
+        const timestamp = Date.now() 
+        return new this({
+            timestamp,
+            lastHash: lastBlock.hash,
+            hash: cryptoHash(timestamp, data, lastBlock.hash),
+            data: data
+        });
+    }
+}
+
+module.exports = Block;
