92eefe77c24b24329b6545fc3bb6d3c080f065e3
Chris Meyering <christophe.meyering@gmail.com>
2020-02-11 17:05:52 -0800

    implemented isValidChain function

--- blockchain.js
+++ blockchain.js
@@ -1,15 +1,40 @@
 const Block = require('./block');
+const cryptoHash = require('./crypto-hash');
 
 class Blockchain {
+
+    //initialize blockchain with genesis block
     constructor() {
         this.chain = [Block.genesis()];
     }
 
+    //adds a new block containing `data` to the chain
     addBlock({ data }) {
         const newBlock = Block.mineBlock({
             lastBlock: this.chain[this.chain.length - 1],
             data})
         this.chain.push(newBlock);
+    }
+
+    //validate chain
+    static isValidChain(chain) {
+        //verify that first block is genesis block
+        if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) {
+            return false;
+        }
+        for (let i = 1; i < chain.length; i++) {
+            const {timestamp, lastHash, hash, data} = chain[i];
+            //validate block's lastHash
+            if (lastHash !== chain[i-1].hash) {
+                return false;
+            }
+            const expectedHash = cryptoHash(data, timestamp, lastHash);
+            //validate block's hash
+            if (hash !== expectedHash) {
+                return false;
+            }
+        }
+        return true;
     }
 }
 
