7afb492c58e076a3056095ff6daff4b1d7193304
Chris Meyering <christophe.meyering@gmail.com>
2020-02-23 22:56:17 -0800

    implemented clearBlockchainTransactions() and validTransactions()

--- src/wallet/transaction-pool.js
+++ src/wallet/transaction-pool.js
@@ -1,3 +1,5 @@
+const Transaction = require('./transaction');
+
 class TransactionPool {
     constructor () {
         this.transactionMap = {};
@@ -10,6 +12,26 @@ class TransactionPool {
     existingTransaction({ inputAddress }) {
         return Object.values(this.transactionMap).
             find((transaction) => transaction.input.address === inputAddress);
+    }
+
+    validTransactions() {
+        return Object.values(this.transactionMap).
+            filter((transaction) => Transaction.isValidTransaction(transaction));
+    }
+
+    clear() {
+        this.transactionMap = {};
+    }
+
+    clearBlockchainTransactions({ chain }) {
+        for (let i = 1; i < chain.length; i++) {
+            const block = chain[i];
+            for (const transaction of block.data) {
+                if (this.transactionMap[transaction.id]) {
+                    Reflect.deleteProperty(this.transactionMap, transaction.id);
+                }
+            }
+        }
     }
 }
 
