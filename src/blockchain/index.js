d7292011a04377e3916d511d632953ba0904161f
Chris Meyering <christophe.meyering@gmail.com>
2020-02-23 23:00:30 -0800

    implemented validTransactionData() and updated replaceChain()  logic

--- src/blockchain/index.js
+++ src/blockchain/index.js
@@ -1,4 +1,8 @@
 const Block = require('./block');
+const Transaction = require('../wallet/transaction');
+const Wallet = require('../wallet');
+
+const { REWARD_INPUT, MINING_REWARD } = require('../config/wallet');
 
 class Blockchain {
 
@@ -14,6 +18,65 @@ class Blockchain {
             data
         });
         this.chain.push(newBlock);
+    }
+
+    replaceChain(newChain, onSuccess) {
+        const replaceFailed = (reason) => console.error(`Failed to replace chain: ${reason}`);
+
+        if (newChain.length <= this.chain.length) {
+            replaceFailed('the incoming chain must be longer');
+        } else if (Blockchain.isValidChain(newChain) === false) {
+            replaceFailed('the incoming chain contains invalid block(s)');
+        } else if (!this.validTransactionData({ chain: newChain })) {
+            replaceFailed('the incoming chain has invalid transaction data');
+        } else {
+            if (onSuccess) {
+                onSuccess();
+            }
+            this.chain = newChain;
+        }
+    }
+
+    validTransactionData({ chain }) {
+        for (const block of chain) {
+            let rewardTransactionCount = 0;
+            const transactionSet = new Set();
+            for (const transaction of block.data) {
+                if (transaction.input.address === REWARD_INPUT.address) {
+                    rewardTransactionCount += 1;
+                    if (rewardTransactionCount > 1) {
+                        console.error('Miner rewards exceed limit');
+                        return false;
+                    }
+                    if (Object.values(transaction.outputMap)[0] !== MINING_REWARD) {
+                        console.error('Miner reward amount is invalid');
+                        return false;
+                    }
+                } else {
+                    if (!Transaction.isValidTransaction(transaction)) {
+                        console.error('Invalid transaction');
+                        return false;
+                    }
+                    const trueBalance = Wallet.calculateBalance({
+                        chain: this.chain,
+                        address: transaction.input.address
+                    });
+
+                    if (transaction.input.amount !== trueBalance) {
+                        console.error('Invalid input amount');
+                        return false;
+                    }
+
+                    if (transactionSet.has(transaction)) {
+                        console.error('Duplicate transaction found in block');
+                        return false;
+                    }
+                    transactionSet.add(transaction);
+
+                }
+            }
+        }
+        return true;
     }
 
     //validate chain
@@ -34,18 +97,6 @@ class Blockchain {
             }
         }
         return true;
-    }
-
-    replaceChain(newChain) {
-        const replaceFailed = (reason) => console.error(`Failed to replace chain: ${reason}`);
-
-        if (newChain.length <= this.chain.length) {
-            replaceFailed('the incoming chain must be longer');
-        } else if (Blockchain.isValidChain(newChain) === false) {
-            replaceFailed('the incoming chain contains invalid block(s)');
-        } else {
-            this.chain = newChain;
-        }
     }
 }
 
