7263ee99010208ffb9cece5152e6a7276b0decf8
Chris Meyering <christophe.meyering@gmail.com>
2020-02-23 22:59:04 -0800

    config for wallet

--- src/config/wallet.js
+++ src/config/wallet.js
@@ -1,3 +1,11 @@
 const STARTING_BALANCE = 1000;
 
-module.exports = { STARTING_BALANCE };
+const REWARD_INPUT = { address: '*authorized-reward*' };
+
+const MINING_REWARD = 50;
+
+module.exports = {
+    STARTING_BALANCE,
+    REWARD_INPUT,
+    MINING_REWARD
+};
